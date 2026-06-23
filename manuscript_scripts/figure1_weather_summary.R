33d1d3dfb7dc2094aea7086efa41301b9cf8291d
CourtneyCampany <courtneycampany@gmail.com>
2026-06-23 10:51:45 -0400

    figure 1 complete for manuscript

--- manuscript_scripts/figure1_weather_summary.R
+++ manuscript_scripts/figure1_weather_summary.R
@@ -1,4 +1,4 @@
--
+
 # Charles Town urban tree physiology study
 # Baseline establishment-period weather figure
 
@@ -117,15 +117,14 @@ temperature_panel <- ggplot(
 
 temperature_panel
 
-ggsave(
-  filename = "figures/weather_temperature_panel.png",
-  plot = temperature_panel,
-  width = 7,
-  height = 3,
-  units = "in",
-  dpi = 300
-)
-
+# ggsave(
+#   filename = "figures/weather_temperature_panel.png",
+#   plot = temperature_panel,
+#   width = 7,
+#   height = 3,
+#   units = "in",
+#   dpi = 300
+# )
 
 
 # Step: Daily VPD panel-----
@@ -182,71 +181,71 @@ vpd_panel <- ggplot(
 
 vpd_panel
   
-ggsave(
-  filename = "figures/weather_VPD_panel.png",
-  plot = vpd_panel,
-  width = 7,
-  height = 2.5,
-  units = "in",
-  dpi = 300
-)  
+# ggsave(
+#   filename = "figures/weather_VPD_panel.png",
+#   plot = vpd_panel,
+#   width = 7,
+#   height = 2.5,
+#   units = "in",
+#   dpi = 300
+# )  
 
 
 # Step: Daily Precipitation panel (not used)-----
 
-# precipitation_panel <- ggplot(
-#   weather_establishment_plot,
-#   aes(x = date)
-# ) +
-#   # Shade the sustained late-July hot period
-#   geom_rect(
-#     data = hot_period_shading,
-#     aes(
-#       xmin = xmin,
-#       xmax = xmax,
-#       ymin = ymin,
-#       ymax = ymax
-#     ),
-#     inherit.aes = FALSE,
-#     fill = "grey92"
-#   ) +
-#   
-#   # Daily measurable precipitation recorded at the NOAA station
-#   geom_col(
-#     aes(y = precip_mm),
-#     width = 0.85,
-#     fill = "grey35"
-#   ) +
-#   
-#   # Beginning of repeated physiological measurements
-#   geom_vline(
-#     xintercept = first_measurement_date,
-#     linetype = "dashed",
-#     linewidth = 0.4
-#   ) +
-#   
-#   scale_x_date(
-#     limits = c(planting_date, final_measurement_date),
-#     date_breaks = "2 weeks",
-#     date_labels = "%b %d",
-#     expand = expansion(mult = c(0.005, 0.005))
-#   ) +
-#   
-#   scale_y_continuous(
-#     limits = c(0, NA),
-#     expand = expansion(mult = c(0, 0.05))
-#   ) +
-#   
-#   labs(
-#     x = NULL,
-#     y = "Precipitation (mm)",
-#     title = "C"
-#   ) +
-#   
-#   weather_theme
-# 
-# precipitation_panel
-# 
+precipitation_panel <- ggplot(
+  weather_establishment_plot,
+  aes(x = date)
+) +
+  # Shade the sustained late-July hot period
+  geom_rect(
+    data = hot_period_shading,
+    aes(
+      xmin = xmin,
+      xmax = xmax,
+      ymin = ymin,
+      ymax = ymax
+    ),
+    inherit.aes = FALSE,
+    fill = "grey92"
+  ) +
+
+  # Daily measurable precipitation recorded at the NOAA station
+  geom_col(
+    aes(y = precip_mm),
+    width = 0.85,
+    fill = "grey35"
+  ) +
+
+  # Beginning of repeated physiological measurements
+  geom_vline(
+    xintercept = first_measurement_date,
+    linetype = "dashed",
+    linewidth = 0.4
+  ) +
+
+  scale_x_date(
+    limits = c(planting_date, final_measurement_date),
+    date_breaks = "2 weeks",
+    date_labels = "%b %d",
+    expand = expansion(mult = c(0.005, 0.005))
+  ) +
+
+  scale_y_continuous(
+    limits = c(0, NA),
+    expand = expansion(mult = c(0, 0.05))
+  ) +
+
+  labs(
+    x = NULL,
+    y = "Precipitation (mm)",
+    title = "C"
+  ) +
+
+  weather_theme
+
+precipitation_panel
+
 # ggsave(
 #   filename = "figures/weather_precipitation_panel_draft.png",
 #   plot = precipitation_panel,
@@ -257,25 +256,119 @@ ggsave(
 # )
 
 
-
 # Step: Precipitation + Supplemental irrigation panel (not used)-----
+
+irrigation_marker_y <- max(weather_establishment_plot$precip_mm, na.rm = TRUE) * 1.08
+
+irrigation_plot_data <- weather_establishment_plot |>
+  filter(irrigated_today) |>
+  mutate(
+    irrigation_marker_y = irrigation_marker_y,
+    irrigation_type = case_when(
+      irrigation_schedule_basis == "shifted_from_Monday_federal_holiday" ~
+        "Holiday-shifted irrigation",
+      TRUE ~
+        "Scheduled irrigation"
+    )
+  )
+
+
+precipitation_panel2 <- ggplot(
+  weather_establishment_plot,
+  aes(x = date)
+) +
+  # Shade the sustained late-July hot period
+  geom_rect(
+    data = hot_period_shading,
+    aes(
+      xmin = xmin,
+      xmax = xmax,
+      ymin = ymin,
+      ymax = ymax
+    ),
+    inherit.aes = FALSE,
+    fill = "grey92"
+  ) +
+
+  # Daily measurable precipitation recorded at the NOAA station
+  geom_col(
+    aes(y = precip_mm),
+    width = 0.85,
+    fill = "grey35"
+  ) +
+
+  # Supplemental irrigation-event markers.
+  # Marker height is graphical only; irrigation was applied as
+  # 18.93 L tree-1 and is not expressed as rainfall-equivalent mm.
+  geom_point(
+    data = irrigation_plot_data,
+    aes(
+      x = date,
+      y = irrigation_marker_y
+    ),
+    inherit.aes = FALSE,
+    shape = 25,
+    size = 2.5,
+    fill = "black",
+    color = "black"
+  ) +
+
+  # Beginning of repeated physiological measurements
+  geom_vline(
+    xintercept = first_measurement_date,
+    linetype = "dashed",
+    linewidth = 0.4
+  ) +
+
+  scale_x_date(
+    limits = c(planting_date, final_measurement_date),
+    date_breaks = "2 weeks",
+    date_labels = "%b %d",
+    expand = expansion(mult = c(0.005, 0.005))
+  ) +
+
+  scale_y_continuous(
+    limits = c(0, irrigation_marker_y * 1.07),
+    expand = expansion(mult = c(0, 0))
+  ) +
+
+  # Manual in-panel irrigation key
+  annotate(
+    "point",
+    x = as.Date("2022-07-04"),
+    y = 25,
+    shape = 25,
+    size = 2.5,
+    fill = "black",
+    color = "black"
+  ) +
+  
+  annotate(
+    "text",
+    x = as.Date("2022-07-07"),
+    y = 25,
+    label = "irrigation",
+    hjust = 0,
+    vjust = 0.5,
+    size = 3.2
+  ) +
+
+  labs(
+    x = "Date",
+    y = "Precipitation (mm)",
+    title = "C"
+  ) +
+
+  weather_theme
+
+precipitation_panel2
+
+
+# Step: Precipitation + Supplemental water drop irrigation panel (not used)-----
 
 # irrigation_marker_y <- max(weather_establishment_plot$precip_mm, na.rm = TRUE) * 1.08
 # 
-# irrigation_plot_data <- weather_establishment_plot |>
-#   filter(irrigated_today) |>
-#   mutate(
-#     irrigation_marker_y = irrigation_marker_y,
-#     irrigation_type = case_when(
-#       irrigation_schedule_basis == "shifted_from_Monday_federal_holiday" ~
-#         "Holiday-shifted irrigation",
-#       TRUE ~
-#         "Scheduled irrigation"
-#     )
-#   )
-# 
-# 
-# precipitation_panel2 <- ggplot(
+# precipitation_panel3 <- ggplot(
 #   weather_establishment_plot,
 #   aes(x = date)
 # ) +
@@ -302,17 +395,15 @@ ggsave(
 #   # Supplemental irrigation-event markers.
 #   # Marker height is graphical only; irrigation was applied as
 #   # 18.93 L tree-1 and is not expressed as rainfall-equivalent mm.
-#   geom_point(
+#   geom_text(
 #     data = irrigation_plot_data,
 #     aes(
 #       x = date,
-#       y = irrigation_marker_y
+#       y = irrigation_marker_y,
+#       label = "\U0001F4A7"
 #     ),
 #     inherit.aes = FALSE,
-#     shape = 25,
-#     size = 2.5,
-#     fill = "black",
-#     color = "black"
+#     size = 4
 #   ) +
 #   
 #   # Beginning of repeated physiological measurements
@@ -336,9 +427,9 @@ ggsave(
 #   
 #   annotate(
 #     "text",
-#     x = planting_date + 2,
-#     y = irrigation_marker_y,
-#     label = "\u25BC  irrigation",
+#     x = planting_date + 60,
+#     y = irrigation_marker_y-6,
+#     label = "\U0001F4A7  irrigation",
 #     hjust = 0,
 #     vjust = -0.2,
 #     size = 3.2
@@ -352,99 +443,16 @@ ggsave(
 #   
 #   weather_theme
 # 
-# precipitation_panel2
+# precipitation_panel3
 
-
-
-# Step: Precipitation + Supplemental water drop irrigation panel-----
-
-irrigation_marker_y <- max(weather_establishment_plot$precip_mm, na.rm = TRUE) * 1.08
-
-precipitation_panel3 <- ggplot(
-  weather_establishment_plot,
-  aes(x = date)
-) +
-  # Shade the sustained late-July hot period
-  geom_rect(
-    data = hot_period_shading,
-    aes(
-      xmin = xmin,
-      xmax = xmax,
-      ymin = ymin,
-      ymax = ymax
-    ),
-    inherit.aes = FALSE,
-    fill = "grey92"
-  ) +
-  
-  # Daily measurable precipitation recorded at the NOAA station
-  geom_col(
-    aes(y = precip_mm),
-    width = 0.85,
-    fill = "grey35"
-  ) +
-  
-  # Supplemental irrigation-event markers.
-  # Marker height is graphical only; irrigation was applied as
-  # 18.93 L tree-1 and is not expressed as rainfall-equivalent mm.
-  geom_text(
-    data = irrigation_plot_data,
-    aes(
-      x = date,
-      y = irrigation_marker_y,
-      label = "\U0001F4A7"
-    ),
-    inherit.aes = FALSE,
-    size = 4
-  ) +
-  
-  # Beginning of repeated physiological measurements
-  geom_vline(
-    xintercept = first_measurement_date,
-    linetype = "dashed",
-    linewidth = 0.4
-  ) +
-  
-  scale_x_date(
-    limits = c(planting_date, final_measurement_date),
-    date_breaks = "2 weeks",
-    date_labels = "%b %d",
-    expand = expansion(mult = c(0.005, 0.005))
-  ) +
-  
-  scale_y_continuous(
-    limits = c(0, irrigation_marker_y * 1.07),
-    expand = expansion(mult = c(0, 0))
-  ) +
-  
-  annotate(
-    "text",
-    x = planting_date + 60,
-    y = irrigation_marker_y-6,
-    label = "\U0001F4A7  irrigation",
-    hjust = 0,
-    vjust = -0.2,
-    size = 3.2
-  ) +
-  
-  labs(
-    x = "Date",
-    y = "Precipitation (mm)",
-    title = "C"
-  ) +
-  
-  weather_theme
-
-precipitation_panel3
-
-ggsave(
-  filename = "figures/weather_precipitation_panel.png",
-  plot = precipitation_panel,
-  width = 7,
-  height = 2.5,
-  units = "in",
-  dpi = 300
-)
+# ggsave(
+#   filename = "figures/weather_precipitation_panel.png",
+#   plot = precipitation_panel,
+#   width = 7,
+#   height = 2.5,
+#   units = "in",
+#   dpi = 300
+# )
 
 
 #Step: Make panel figure-----
@@ -453,19 +461,21 @@ ggsave(
 temperature_panel_combined <- temperature_panel +
   theme(
     axis.text.x = element_blank(),
-    axis.ticks.x = element_blank(),
-    axis.title.x = element_blank()
+    axis.title.x = element_blank(),
+    axis.ticks.x = element_line(linewidth = 0.4),
+    axis.ticks.length.x = unit(0.10, "cm")
   )
 
 vpd_panel_combined <- vpd_panel +
   theme(
     axis.text.x = element_blank(),
-    axis.ticks.x = element_blank(),
-    axis.title.x = element_blank()
+    axis.title.x = element_blank(),
+    axis.ticks.x = element_line(linewidth = 0.4),
+    axis.ticks.length.x = unit(0.10, "cm")
   )
 
 # Retain date-axis labels only on the bottom panel
-precipitation_panel_combined <- precipitation_panel3 +
+precipitation_panel_combined <- precipitation_panel2 +
   labs(x = "Date")
 
 
@@ -474,10 +484,11 @@ weather_summary_figure <-
   vpd_panel_combined /
   precipitation_panel_combined +
   plot_layout(
-    heights = c(1.3, 1, 1.15)
+    heights = c(1.1, 1, 1.1)
   )
 
-weather_summary_figure
+# windows()
+# weather_summary_figure
 
 ggsave(
   filename = "figures/CharlesTown_weather_summary_figure_draft.png",
@@ -499,4 +510,4 @@ ggsave(
 
 ##water drop likely will not render on PDF, go back to basic symbol
 
-##if I want to remove the starting measurment date, comment out "geom_vline"
+##if I want to remove the starting measurement date, comment out "geom_vline"
